6c68d28d12f5ad96153ba284cc0927d1b517342f
Bobomurod <bobomurodamirov.b@gmail.com>
2025-01-24 18:18:23 +0500

    PRoducsts page

--- resources/views/products.php
+++ resources/views/products.php
@@ -5,7 +5,75 @@ views("components/header");
 views("components/navbar");
 ?>
 
-<h1 class="text-red-700 align-middle">Loyiha hali yankunlamdi</h1>
+<div class="container flex flex-wrap justify-center gap-4 mx-auto w-full overflow-x-hidden">
+    <div class="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
+        <div class="relative overflow-hidden rounded-t-lg">
+            <img src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+                 alt="card-image"
+                 class="object-cover w-full h-80 rounded-t-lg"/>
+        </div>
+        <div class="p-4 flex flex-col items-center justify-center text-center">
+            <h6 class="mb-2 text-slate-800 text-xl font-semibold">Website Review Check</h6>
+            <h5 class="mb-2 text-slate-800 bg-yellow-400 text-xl font-semibold rounded-lg px-4 py-2">Website Review Check</h5>
+        </div>
+    </div>
+    <div class="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
+        <div class="relative overflow-hidden rounded-t-lg">
+            <img src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+                 alt="card-image"
+                 class="object-cover w-full h-80 rounded-t-lg"/>
+        </div>
+        <div class="p-4 flex flex-col items-center justify-center text-center">
+            <h6 class="mb-2 text-slate-800 text-xl font-semibold">Website Review Check</h6>
+            <h5 class="mb-2 text-slate-800 bg-yellow-400 text-xl font-semibold rounded-lg px-4 py-2">Website Review Check</h5>
+        </div>
+    </div>
+    <div class="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
+        <div class="relative overflow-hidden rounded-t-lg">
+            <img src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+                 alt="card-image"
+                 class="object-cover w-full h-80 rounded-t-lg"/>
+        </div>
+        <div class="p-4 flex flex-col items-center justify-center text-center">
+            <h6 class="mb-2 text-slate-800 text-xl font-semibold">Website Review Check</h6>
+            <h5 class="mb-2 text-slate-800 bg-yellow-400 text-xl font-semibold rounded-lg px-4 py-2">Website Review Check</h5>
+        </div>
+    </div>
+    <div class="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
+        <div class="relative overflow-hidden rounded-t-lg">
+            <img src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+                 alt="card-image"
+                 class="object-cover w-full h-80 rounded-t-lg"/>
+        </div>
+        <div class="p-4 flex flex-col items-center justify-center text-center">
+            <h6 class="mb-2 text-slate-800 text-xl font-semibold">Website Review Check</h6>
+            <h5 class="mb-2 text-slate-800 bg-yellow-400 text-xl font-semibold rounded-lg px-4 py-2">Website Review Check</h5>
+        </div>
+    </div>
+    <div class="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
+        <div class="relative overflow-hidden rounded-t-lg">
+            <img src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+                 alt="card-image"
+                 class="object-cover w-full h-80 rounded-t-lg"/>
+        </div>
+        <div class="p-4 flex flex-col items-center justify-center text-center">
+            <h6 class="mb-2 text-slate-800 text-xl font-semibold">Website Review Check</h6>
+            <h5 class="mb-2 text-slate-800 bg-yellow-400 text-xl font-semibold rounded-lg px-4 py-2">Website Review Check</h5>
+        </div>
+    </div>
+    <div class="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
+        <div class="relative overflow-hidden rounded-t-lg">
+            <img src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+                 alt="card-image"
+                 class="object-cover w-full h-80 rounded-t-lg"/>
+        </div>
+        <div class="p-4 flex flex-col items-center justify-center text-center">
+            <h6 class="mb-2 text-slate-800 text-xl font-semibold">Website Review Check</h6>
+            <h5 class="mb-2 text-slate-800 bg-yellow-400 text-xl font-semibold rounded-lg px-4 py-2">Website Review Check</h5>
+        </div>
+    </div>
+</div>
+
 
 <?php
 views("components/footer");
